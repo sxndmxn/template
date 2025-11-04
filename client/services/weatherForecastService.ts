275ed66418af3099455af255096355d164514760
sxndmxn <manuel-lafuente@protonmail.com>
2025-11-04 15:05:11 -0800

    changing client name to server

--- client/services/weatherForecastService.ts
+++ client/services/weatherForecastService.ts
@@ -5,9 +5,6 @@ type WeatherForecast = components["schemas"]["WeatherForecast"];
 type WeatherForecastList = WeatherForecast[];
 
 export async function getWeatherForecast(signal?: AbortSignal): Promise<WeatherForecastList> {
-    const { data, error } = await api.GET("/WeatherForecast", { signal });
-    if (error) {
-        throw error;
-    }
+    const { data } = await api.GET("/WeatherForecast", { signal });
     return data ?? [];
 }
--- server/Controllers/WeatherForecastController.cs
+++ server/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace Api.Controllers;
+namespace Server.Controllers;
 
 [ApiController]
 [Route("[controller]")]
